3249e5094840536122bd02b5f14f9aa777dbd7d4
NurlanAslnzade <nurlan.aslanzade20@gmail.com>
2025-11-25 11:56:04 +0000

    sd

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,4 @@
-#!/usr/bin/python3
-str1 = "Holberton"
-str2 = "School"
-print(f"Welcome to {str1}")
-print(f"{str2}!")
+1. #!/usr/bin/python3
+2. str1 = "Holberton"
+3. str2 = "School"
+4. print(f"Welcome to {str1} {str2}!")
