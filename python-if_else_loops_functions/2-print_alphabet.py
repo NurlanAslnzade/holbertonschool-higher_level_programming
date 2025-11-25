7e130dd9be42892c31adb4b2beb603b445746ba0
NurlanAslnzade <nurlan.aslanzade20@gmail.com>
2025-11-25 14:15:04 +0000

    s

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-a = ''
+a = ""
 for i in range (97,123):
-    if chr(i) != 'e' and chr(i) !='q':
+    if chr(i) != "e" and chr(i) != "q":
         a += chr(i)
-print('{}'.format(a),end="")
+print("{}".format(a), end="")
