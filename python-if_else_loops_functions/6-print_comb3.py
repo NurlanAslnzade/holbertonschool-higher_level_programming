5f442d387a26d803fe0148de850f27b37d879b73
NurlanAslnzade <nurlan.aslanzade20@gmail.com>
2025-11-26 11:54:53 +0000

    s

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(0,10):
-    for h in range(i+1,10):
+for i in range(0, 10):
+    for h in range(i+1, 10):
         if i*10 + h !=89:
             print("{0:02d}".format(i*10 + h), end=(", "))
 print(89)
