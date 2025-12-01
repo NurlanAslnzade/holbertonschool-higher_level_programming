1285c51d5a7ed6ee7c1696f99f8ca6b921a68954
NurlanAslnzade <nurlan.aslanzade20@gmail.com>
2025-12-01 12:03:58 +0000

    s

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -6,8 +6,8 @@ def main():
     a = 1
     b = 2
     c = add(a, b)
+    print("{:d} + {:d} = {:d}".format(a, b, c))
 
 
-   print("{:d} + {:d} = {:d}".format(a, b, c))
 if __name__ == "__main__":
     main()
