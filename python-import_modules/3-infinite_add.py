d6040eabba59f795132d4053f0aa79c431fdc4be
NurlanAslnzade <nurlan.aslanzade20@gmail.com>
2025-12-01 13:51:02 +0000

    s

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ = "__main__":
+
+
+    import sys
+    a = 0
+    if len(sys.argv) == 1:
+        print("0")
+    else:
+        for i in range(1,sys.argv):
+            a += int(sys.argv[1]
+        print("{}".format(a))
+
