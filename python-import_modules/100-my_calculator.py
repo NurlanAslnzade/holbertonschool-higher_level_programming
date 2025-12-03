90bbd4f66f79da6481e3573b622527d1b89821a4
NurlanAslnzade <nurlan.aslanzade20@gmail.com>
2025-12-03 19:42:46 +0000

    s

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
 def add(a, b):
+    a = int()
+    b = int()
     return (a + b)
 def sub(a, b):
+    a = int()
+    b = int()
     return (a - b)
 def mul(a, b):
+    a = int()
+    b = int()
     return (a * b)
 def div(a, b):
-    return (a / b)
+    a = int()
+    b = int()
+    return int(a / b)
