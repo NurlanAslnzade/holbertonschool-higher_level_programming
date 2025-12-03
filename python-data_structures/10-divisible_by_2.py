45b53a0ecbe93775ee79aa03931298abcbbc5a68
dsada <asd>
2025-12-03 13:26:45 +0000

    s

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -2,6 +2,6 @@
 def divisible_by_2(my_list=[]):
     for i in my_list:
         if i % 2 == 0:
-            print("{:d} divisible by 2".format(i))
+            print("{:d} is divisible by 2".format(i))
         else:
             print("{:d} is not divisible by 2".format(i))
