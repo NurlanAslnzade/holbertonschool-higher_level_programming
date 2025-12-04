cbc9861009909d4cd2df1b360e013281b36ebb17
NurlanAslnzade <nurlan.aslanzade20@gmail.com>
2025-12-04 13:05:22 +0000

    s

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    a = matrix
-    for i in matrix:
-        for k in range(0,len(i)):
-            i[k] = i[k]**2
-    print(matrix)
-    print(a)
+    return [[ i ** 2 for i in a] for a in matrix]
