33561450ca735ae7514a6095fabb8a8c7520e97a
dsad <asda>
2025-12-05 14:21:09 +0000

    s

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     a = []
-    for key, value in a_dictionary:
+    for key, value in a_dictionary.items():
         a.append(value)
     if len(a) > 0:
         return max(a)
