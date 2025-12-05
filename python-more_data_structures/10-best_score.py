9d8860bb393bd616fc279e3e5080f0f697e317e5
dsad <asda>
2025-12-05 14:32:27 +0000

    s

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    a = []
-    for key, value in a_dictionary.items():
-        a.append(value)
-    if len(a) > 0:
-        return max(a)
-    else:
+    if not a_dictionary:
         return None
+    else:
+        a = max(a_dictionary, key=a_dictionary.get)
+        return a
