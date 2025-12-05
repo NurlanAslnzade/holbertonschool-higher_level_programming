1114c6b9a47244bea24ffd2cad98a71b0ed4d3dc
dsad <asda>
2025-12-05 13:52:03 +0000

    s

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    for i in sorted(a_dictionary):
-        print(f"{i}: {a_dictionary[i]}")
+    for key in sorted(a_dictionary):
+        print(f"{key}: {a_dictionary[key]}")
 
