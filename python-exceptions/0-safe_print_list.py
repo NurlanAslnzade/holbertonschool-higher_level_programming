a17e4a93100b8c0c40a221a9963d377b3fcc0eef
sada <asda>
2025-12-06 13:15:34 +0000

    s

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 
 def safe_print_list(my_list=[], x=0):
     a = 0
-    for i in range(my_list):
+    for i in range(x):
         try:
             print('{:d}'.format(i), end = '')
             a += 1
