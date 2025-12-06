38521d9e79ba73b76094a903486531863911f22e
sada <asda>
2025-12-06 12:51:18 +0000

    s

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    a = 0
+    for i in my_list:
+        try:
+            print('{:d}'.format(i))
+            a += 1
+        except IndexError:
+            break
+    return a
