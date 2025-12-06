7cf15299b1b8b0aad83df18ee389f9feda73208e
sada <asda>
2025-12-06 13:19:30 +0000

    s

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,12 +1,10 @@
-#!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
-    a = 0
-    for i in range(1, x+1):
+    count = 0
+    for i in range(x):
         try:
-            print('{:d}'.format(i), end = '')
-            a += 1
+            print(my_list[i], end="")
+            count += 1
         except IndexError:
             break
     print()
-    return a
+    return count
