86bcc51dc9335c14606786ace179d3a7f4bdf5fa
sada <asda>
2025-12-06 14:56:39 +0000

    s

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -2,10 +2,9 @@
 
 def safe_print_division(a, b):
     try:
-        if b != 0:
-            result = a/b
+        result = a / b
     except:
         result = None
     finally:
-        print("Inside result:", result)
-        print("{:d} / {:d} = {}".format(a, b, result))
+        print("Inside result: {}".format(result))
+    return result
