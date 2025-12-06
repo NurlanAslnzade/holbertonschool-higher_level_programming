3fa18b6e2331f955306ba9af3b138c0b9bdd127e
sada <asda>
2025-12-06 13:41:30 +0000

    s

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
     a = 0
     for i in range(x):
         try:
-            print(("{}".format(i))
+            print(("{:d}".format(i))
             a += 1
         except (Type Error):
             pass
