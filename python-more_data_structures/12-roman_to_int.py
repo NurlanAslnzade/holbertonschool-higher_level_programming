03cc3faf8eaf4021bec19a818ab8ab9c47695373
dsadsa <asda>
2025-12-07 10:41:24 +0000

    s

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+
+    a = 0
+    b = 0
+
+    rum_number = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 1000
+    }
+
+    for i in reversed(roman_string):
+        value = rum_number.get(i, 0)
+        if value < b:
+            a -= value
+        else:
+            a += value
+        b = value
+
+    return a
+
+print(roman_to_int(roman_string)
