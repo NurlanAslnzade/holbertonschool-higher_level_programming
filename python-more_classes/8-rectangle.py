1cce5d0fe36813621cac0d26a8589eee2e0f331e
d <sadasd>
2025-12-10 12:46:42 +0000

    s

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -57,9 +57,9 @@ class Rectangle:
 
     def __repr__(self):
         return "Rectangle({}, {})".format(self.__width, self.__height)
- 
+
     def __del__(self):
-        print("Bye rectangle...")  
+        print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
         del self
 
