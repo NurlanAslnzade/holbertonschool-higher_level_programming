8d6f9f7b19ea42b24c7975e5ee84b7fe26762545
dsadas <sadsa>
2025-12-14 11:57:53 +0000

    s

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-"""nese olsun"""
-Rectangle = __import__("9-rectangle.py").Rectangle
+"""nese"""
+
+Rectangle = __import__("9-rectangle").Rectangle
 
 
 class Square(Rectangle):
-    """sadasd"""
+    """nese"""
 
     def __init__(self, size):
         self.integer_validator("size", size)
         super().__init__(size, size)
 
     def area(self):
-        return self._Rectangle__width ** 2
+        return super().area()
