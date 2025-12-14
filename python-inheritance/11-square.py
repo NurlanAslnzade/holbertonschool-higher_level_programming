70c0b407a9a7678dd8e74dc14877e50e47e8206a
dsadas <sadsa>
2025-12-14 12:02:24 +0000

    s

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""nese"""
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """nese"""
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def area(self):
+        return super().area()
+
+   def __str__(self):
+    return "[Square] {}/{}".format(self._Rectangle__width, self._Rectangle__height)
