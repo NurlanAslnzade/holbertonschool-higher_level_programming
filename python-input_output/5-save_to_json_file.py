6f793bcdb5b2d8d2861fcbf3fe65b941dd131f25
sdaas <sdaas>
2025-12-14 16:50:03 +0000

    s

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""using JSON represantion"""
+import json as j
+
+
+def save_to_json_file(my_obj, filename):
+    """JSON represantion"""
+    with open(filename, "w") as f:
+        j.dump(my_obj, f)
---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""represantion with data structure"""
+
+
+def class_to_json(obj):
+    """represantion"""
+    return obj.__dict__
