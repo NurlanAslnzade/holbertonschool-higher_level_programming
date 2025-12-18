db3d0044cc21a01ebda0ca9a8686c73782c504f7
dasdsa <sda>
2025-12-18 21:52:42 +0000

    s

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-import json  as j
+import json
 
 
-def serialize_and_save_to_file(filename, data):
-    with open(filename, 'w') as f:
-        j.dump(data, filename)
+def serialize_and_save_to_file(data, filename):
+    with open(filename, "w") as f:
+        json.dump(data, f)
 
 def load_and_deserialize(filename):
-    with open(filename, 'r') as f:
-        data = j.load(f)
-        return(data)
+    with open(filename, "r") as f:
+        data = json.load(f)
+        return data
