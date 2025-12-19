c9932e2daf0d019e54ee6a8ad6f357883732b96a
sada <sada>
2025-12-19 10:20:39 +0000

    s

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import csv
+import json as j
+
+def convert_csv_to_json(filename):
+    try:
+        with open(filename, "r") as f:
+            data = list(csv.DictReader(f))
+
+        with open(filename, "w") as f:
+            data.json = j.dumps(data, f)
+        return True
+    except Exception FileNotFoundError:
+        return False
+    except Exception:
+        return False
